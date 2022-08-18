6b8e45e50efd9d32f410655d4e4600faf7e086c0
sohailabbas11 <sohailabbas669@gmail.com>
2022-08-18 17:31:31 +0200

    basic socket.io

---
+++ script.js
@@ -0,0 +1,36 @@
+const socket = io('http://localhost:3539')
+const messageContainer = document.getElementById('message-container')
+const messageForm = document.getElementById('send-container')
+const messageInput = document.getElementById('message-input')
+
+const name = prompt('what is your name?')
+appendMessage('you joined')
+socket.emit('new-user', name)
+
+
+socket.on('chat-message', data => {
+    appendMessage(`${data.name}: ${data.message}`)
+})
+
+socket.on('user-connected', name => {
+    appendMessage(`${name} connected`)
+})
+
+socket.on('user-disconnected', name => {
+    appendMessage(`${name} disconnected`)
+})
+
+messageForm.addEventListener('submit', e => {
+    e.preventDefault()
+    const message = messageInput.value
+    appendMessage(`you: ${message}`)
+    socket.emit('send-chat-message', message)
+    messageInput.value = ''
+})
+
+function appendMessage(message) {
+    const messageElement = document.createElement('div')
+    messageElement.innerText = message
+    messageContainer.append(messageElement)
+}
+
